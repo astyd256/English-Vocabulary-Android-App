2c869255d04f105096ac1d4b1f5d70b00936bf67
astyd256 <astyd637@gmail.com>
2023-03-06 07:11:32 +0700

    Third commit. Added onboard intro.

--- app/src/main/java/com/tsu/firstlab/MainActivity.kt
+++ app/src/main/java/com/tsu/firstlab/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.tsu.firstlab
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.tsu.firstlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, OnBoardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
     }
 }
---
+++ app/src/main/java/com/tsu/firstlab/OnBoardActivity.kt
@@ -0,0 +1,65 @@
+package com.tsu.firstlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.tsu.firstlab.databinding.ActivityOnBoardBinding
+
+class OnBoardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnBoardBinding
+    private var currentPageIndex = 0
+
+    private var headerList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imageList = mutableListOf<Int>()
+    private var pagList = mutableListOf<Int>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_board)
+
+        binding = ActivityOnBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addToList(getString(R.string.onboard_header1), getString(R.string.onboard_text1), R.drawable.intro1, R.drawable.pagination1)
+        addToList(getString(R.string.onboard_header2), getString(R.string.onboard_text2), R.drawable.intro2, R.drawable.pagination2)
+        addToList(getString(R.string.onboard_header3), getString(R.string.onboard_text3), R.drawable.intro3, R.drawable.pagination3)
+
+        var viewPager = binding.ViewPager
+        viewPager.adapter = ViewPagerAdapter(headerList, descList, imageList, pagList)
+
+        viewPager?.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            if (position == 2) {
+                binding.buttonNext.text = getString(R.string.onboard_button3)
+            }
+            else{
+                binding.buttonNext.text = getString(R.string.onboard_button1)
+            }
+        }
+        })
+
+        binding.buttonNext.setOnClickListener {
+            currentPageIndex = viewPager.currentItem
+            if(currentPageIndex < 2) {
+                currentPageIndex++
+                viewPager.setCurrentItem(currentPageIndex, true)
+            }
+        }
+
+        binding.buttonSkip.setOnClickListener{
+
+        }
+    }
+
+    private fun addToList(header: String, description: String, image: Int, pagImage: Int) {
+        headerList.add(header)
+        descList.add(description)
+        imageList.add(image)
+        pagList.add(pagImage)
+
+    }
+}
